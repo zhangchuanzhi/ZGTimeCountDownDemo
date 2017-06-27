d400644174533ac25dc7c5db906f9952bdcc6a4d
zhangzhi0903 <18310127735@163.com>
2017-06-27 09:17:28 +0800

    Initial Commit

---
+++ ZGTimeCountDownDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZGTimeCountDownDemo
+//
+//  Created by offcn_zcz32036 on 2017/6/27.
+//  Copyright © 2017年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZGTimeCountDownDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZGTimeCountDownDemo
+//
+//  Created by offcn_zcz32036 on 2017/6/27.
+//  Copyright © 2017年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
